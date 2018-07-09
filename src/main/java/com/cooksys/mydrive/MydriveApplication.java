dbeaa36a81fc79eef2ec394670449dc025324d4d
Cruz Muniz <kingcruziii@gmail.com>
2018-07-09 14:43:38 -0500

    first commit

---
+++ src/main/java/com/cooksys/mydrive/MydriveApplication.java
@@ -0,0 +1,12 @@
+package com.cooksys.mydrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydriveApplication.class, args);
+	}
+}
